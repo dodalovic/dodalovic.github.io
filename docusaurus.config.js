0163df63f47f8ab5cae66d92f7fcc2119f2042b5
Dusan Odalovic <dodalovic@gmail.com>
2023-03-27 08:23:26 +0200

    Add search

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,106 +9,120 @@ const projectName = 'dodalovic.github.io';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Dusan Odalovic',
-  tagline: 'Senior Software Engineer',
-  favicon: 'img/favicon.ico',
-  staticDirectories: ['public', 'static'],
+    title: 'Dusan Odalovic',
+    tagline: 'Senior Software Engineer',
+    favicon: 'img/favicon.ico',
+    staticDirectories: ['public', 'static'],
 
-  // Set the production url of your site here
-  url: `https://${organizationName}.github.io`,
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: `/`,
+    // Set the production url of your site here
+    url: `https://${organizationName}.github.io`,
+    // Set the /<baseUrl>/ pathname under which your site is served
+    // For GitHub pages deployment, it is often '/<projectName>/'
+    baseUrl: `/`,
 
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName, // Usually your GitHub org/user name.
-  projectName, // Usually your repo name.
-  trailingSlash: false,
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+    // GitHub pages deployment config.
+    // If you aren't using GitHub pages, you don't need these.
+    organizationName, // Usually your GitHub org/user name.
+    projectName, // Usually your repo name.
+    trailingSlash: false,
+    onBrokenLinks: 'throw',
+    onBrokenMarkdownLinks: 'warn',
 
-  // Even if you don't use internalization, you can use this field to set useful
-  // metadata like html lang. For example, if your site is Chinese, you may want
-  // to replace "en" with "zh-Hans".
-  i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
-  },
+    // Even if you don't use internalization, you can use this field to set useful
+    // metadata like html lang. For example, if your site is Chinese, you may want
+    // to replace "en" with "zh-Hans".
+    i18n: {
+        defaultLocale: 'en',
+        locales: ['en'],
+    },
 
-  presets: [
-    [
-      'classic',
-      /** @type {import('@docusaurus/preset-classic').Options} */
-      ({
-        docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-              `https://github.com/${organizationName}/${projectName}/tree/main/`,
-        },
-        blog: {
-          readingTime: ({content, frontMatter, defaultReadingTime}) =>
-              defaultReadingTime({content, options: {wordsPerMinute: 130}}),
-          showReadingTime: true,
-          blogSidebarTitle: 'All posts',
-          blogSidebarCount: 'ALL',
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-              `https://github.com/${organizationName}/${projectName}/tree/main/`,
-        },
-        theme: {
-          customCss: require.resolve('./src/css/custom.css'),
-        },
-      }),
+    presets: [
+        [
+            'classic',
+            /** @type {import('@docusaurus/preset-classic').Options} */
+            ({
+                docs: {
+                    sidebarPath: require.resolve('./sidebars.js'),
+                    // Please change this to your repo.
+                    // Remove this to remove the "edit this page" links.
+                    editUrl:
+                        `https://github.com/${organizationName}/${projectName}/tree/main/`,
+                },
+                blog: {
+                    readingTime: ({content, frontMatter, defaultReadingTime}) =>
+                        defaultReadingTime({content, options: {wordsPerMinute: 130}}),
+                    showReadingTime: true,
+                    blogSidebarTitle: 'All posts',
+                    blogSidebarCount: 'ALL',
+                    // Please change this to your repo.
+                    // Remove this to remove the "edit this page" links.
+                    editUrl:
+                        `https://github.com/${organizationName}/${projectName}/tree/main/`,
+                },
+                theme: {
+                    customCss: require.resolve('./src/css/custom.css'),
+                },
+            }),
+        ],
     ],
-  ],
 
-  themeConfig:
+    themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-    ({
-      zoom: {},
-      // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
-      navbar: {
-        title: 'My Site',
-        logo: {
-          alt: 'My Site Logo',
-          src: 'img/logo.svg',
-        },
-        items: [
-          {to: '/blog', label: 'Blog', position: 'left'},
-          {
-            href: `https://github.com/${organizationName}/${projectName}`,
-            label: 'GitHub',
-            position: 'right',
-          },
-        ],
-      },
-      footer: {
-        style: 'dark',
-        links: [
-          {
-            title: 'More',
-            items: [
-              {
-                label: 'Blog',
-                to: '/blog',
-              }
-            ],
-          },
-        ],
-        copyright: `Copyright © ${new Date().getFullYear()} Dusan Odalovic Website. Built with Docusaurus.`,
-      },
-      prism: {
-        additionalLanguages: ['yaml', 'http', 'json', 'kotlin', 'java'],
-        theme: require('prism-react-renderer/themes/palenight'),
-        darkTheme: require('prism-react-renderer/themes/palenight'),
-      },
-    }),
-  plugins: [require.resolve("docusaurus-plugin-image-zoom")]
+        ({
+            zoom: {},
+            // Replace with your project's social card
+            image: 'img/docusaurus-social-card.jpg',
+            navbar: {
+                title: 'My Site',
+                logo: {
+                    alt: 'My Site Logo',
+                    src: 'img/logo.svg',
+                },
+                items: [
+                    {to: '/blog', label: 'Blog', position: 'left'},
+                    {
+                        href: `https://github.com/${organizationName}/${projectName}`,
+                        label: 'GitHub',
+                        position: 'right',
+                    },
+                ],
+            },
+            footer: {
+                style: 'dark',
+                links: [
+                    {
+                        title: 'More',
+                        items: [
+                            {
+                                label: 'Blog',
+                                to: '/blog',
+                            }
+                        ],
+                    },
+                ],
+                copyright: `Copyright © ${new Date().getFullYear()} Dusan Odalovic Website. Built with Docusaurus.`,
+            },
+            prism: {
+                additionalLanguages: ['yaml', 'http', 'json', 'kotlin', 'java'],
+                theme: require('prism-react-renderer/themes/palenight'),
+                darkTheme: require('prism-react-renderer/themes/palenight'),
+            },
+        }),
+    plugins: [require.resolve("docusaurus-plugin-image-zoom")],
+    themes: [
+        [
+            "@easyops-cn/docusaurus-search-local",
+            /** @type {import("@easyops-cn/docusaurus-search-local").PluginOptions} */
+            ({
+                hashed: true,
+                indexBlog: true,
+                blogRouteBasePath: "/",
+                language: ["en"],
+                highlightSearchTermsOnTargetPage: true,
+                explicitSearchResultPath: true,
+            }),
+        ]
+    ]
 };
 
 module.exports = config;
